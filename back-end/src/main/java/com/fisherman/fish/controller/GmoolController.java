44a63ced4e4337b0a85bc88951274fb7ad3221bd
Jaehwan Hyun <jec0101@naver.com>
2024-01-08 19:27:54 +0900

    createGmool 메서드 선언

--- back-end/src/main/java/com/fisherman/fish/controller/GmoolController.java
+++ back-end/src/main/java/com/fisherman/fish/controller/GmoolController.java
@@ -1,5 +1,6 @@
 package com.fisherman.fish.controller;
 
+import com.fisherman.fish.dto.FileDTO;
 import com.fisherman.fish.dto.GmoolDTO;
 import com.fisherman.fish.service.GmoolService;
 import com.fisherman.fish.service.MemberService;
@@ -20,19 +21,25 @@ public class GmoolController {
     @GetMapping("/")
     public List<GmoolDTO> getGmools(){
         // 모든 그물 반환
-        List<GmoolDTO> gmools = gmoolService.getAllGmools();
+        List<GmoolDTO> gmools = gmoolService.searchAll();
         // TODO: 비어있다면 예외처리
 
         return gmools;
     }
     
     @PostMapping("/")
-    public String createGmool(){
+    public String createGmool(@ModelAttribute("gmool") GmoolDTO gmoolDTO, @ModelAttribute("file") FileDTO file){
         // 받은 정보로 그물 생성
-        // 1. pin 번호 생성
-        // 2. qr코드 생성
-        // 3. 링크 생성
-        // 셋 다 묶어서 반환
+        //  1. pin 번호 생성
+        //  2. qr코드 생성
+        //  3. 링크 생성
+        //  셋 다 묶어서 반환
+        // 해당 그물 저장
+        int pinNumber = GmoolService.generatePinNumber();
+        gmoolDTO.setPinNumber(pinNumber);
+        // TODO: 첨부 파일 수를 여기서 세서 그물dto에 채워줘야 할듯
+        // qr코드랑 링크 생성했다고 가정
+        gmoolService.save(gmoolDTO);
         return "ok";
     }
 

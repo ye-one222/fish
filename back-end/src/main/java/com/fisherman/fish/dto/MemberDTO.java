5c6ff78c348ca33f9f3d3bab5bab5b3a50bdc064
Jaehwan Hyun <jec0101@naver.com>
2024-01-08 19:21:45 +0900

    필드명 수정 (member 삭제)

--- back-end/src/main/java/com/fisherman/fish/dto/MemberDTO.java
+++ back-end/src/main/java/com/fisherman/fish/dto/MemberDTO.java
@@ -1,8 +1,14 @@
 package com.fisherman.fish.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MemberDTO {
-    private String memberId; // 아이디
-    private String memberPassword; // 비밀번호
-    private String memberFirstName; // 이름
-    private String memberLastName; // 성
+    private String Id; // 아이디
+    private String Password; // 비밀번호
+    private String FirstName; // 이름
+    private String LastName; // 성
+
 }

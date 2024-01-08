74702e65b3ae15ae4886b40a534c709670f77e58
Jaehwan Hyun <jec0101@naver.com>
2024-01-08 18:59:52 +0900

    필요한 메서드 선언

--- back-end/src/main/java/com/fisherman/fish/service/GmoolService.java
+++ back-end/src/main/java/com/fisherman/fish/service/GmoolService.java
@@ -1,4 +1,23 @@
 package com.fisherman.fish.service;
 
+import com.fisherman.fish.dto.GmoolDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class GmoolService {
+
+    public List<GmoolDTO> getAllGmools() {
+    }
+
+    public Optional<GmoolDTO> searchById(Long gid) {
+    }
+
+    public Optional<GmoolDTO> searchByUserId(Long uid){
+
+    }
 }
--- back-end/src/main/java/com/fisherman/fish/service/MemberService.java
+++ back-end/src/main/java/com/fisherman/fish/service/MemberService.java
@@ -1,4 +1,16 @@
 package com.fisherman.fish.service;
 
+import com.fisherman.fish.dto.MemberDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MemberService {
+    public List<MemberDTO> getAllMembers() {
+    }
+
+    public Optional<MemberDTO> searchById(String id) {
+    }
 }

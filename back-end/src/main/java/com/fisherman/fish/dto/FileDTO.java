ceae518bb1c25fab17dcf0c9f30c0cd87e4c716a
Jaehwan Hyun <jec0101@naver.com>
2024-01-08 19:23:40 +0900

    생성자, Getter Setter 추가

--- back-end/src/main/java/com/fisherman/fish/dto/FileDTO.java
+++ back-end/src/main/java/com/fisherman/fish/dto/FileDTO.java
@@ -1,5 +1,13 @@
 package com.fisherman.fish.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class FileDTO {
     private String originalFileName; // 원래 파일명
     private String storedFileName; // 서버에 저장되는 파일 명

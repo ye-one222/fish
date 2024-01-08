fc9c0755bebfbdf1b5f46391841e650f9d5d0133
Jaehwan Hyun <jec0101@naver.com>
2024-01-08 18:58:23 +0900

    멤버dto 업데이트

--- back-end/src/main/java/com/fisherman/fish/dto/MemberDTO.java
+++ back-end/src/main/java/com/fisherman/fish/dto/MemberDTO.java
@@ -1,4 +1,9 @@
 package com.fisherman.fish.dto;
 
 public class MemberDTO {
+    private Long id;
+    private String memberId;
+    private String memberPassword;
+    private String memberFirstName;
+    private String memberLastName;
 }

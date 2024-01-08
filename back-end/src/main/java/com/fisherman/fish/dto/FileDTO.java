92d54cca03ca8ab4f64403772bd7814aebe21c50
Jaehwan Hyun <jec0101@naver.com>
2024-01-08 18:58:03 +0900

    파일dto 업데이트

--- back-end/src/main/java/com/fisherman/fish/dto/FileDTO.java
+++ back-end/src/main/java/com/fisherman/fish/dto/FileDTO.java
@@ -1,4 +1,9 @@
 package com.fisherman.fish.dto;
 
 public class FileDTO {
+    private String id; // 파일 id -> redundant?
+    private String originalFileName; // 원래 파일명
+    private String storedFileName; // 서버에 저장되는 파일 명
+    private long gmoolId; // 연결된 그물의 id
+    private long fileSize; // 해당 파일 크기
 }

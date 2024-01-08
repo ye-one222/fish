df54902fd7eccac086c388ed2ccbe9805ee2c19b
Jaehwan Hyun <jec0101@naver.com>
2024-01-08 18:58:14 +0900

    그물dto 업데이트

--- back-end/src/main/java/com/fisherman/fish/dto/GmoolDTO.java
+++ back-end/src/main/java/com/fisherman/fish/dto/GmoolDTO.java
@@ -1,4 +1,14 @@
 package com.fisherman.fish.dto;
 
+import java.time.LocalDateTime;
+
 public class GmoolDTO {
+    private long id; // 그물 id
+    private String gmoolName; // 그물명
+    private String password; // 그물 암호
+    private LocalDateTime createdTime; // 생성시각
+    private int dueMinute; // 유효기간 (분)
+    private int pinNumber; // 그물 핀번호
+    private int fileCount; // 첨부 파일 수
+
 }

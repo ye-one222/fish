2429346ddffb23af76a702ede11fdf6f46fdc318
Jaehwan Hyun <jec0101@naver.com>
2024-01-09 20:47:40 +0900

    DTO에 파일도 저장 / toGmoolDTO 작성

--- back-end/src/main/java/com/fisherman/fish/dto/GmoolDTO.java
+++ back-end/src/main/java/com/fisherman/fish/dto/GmoolDTO.java
@@ -1,15 +1,18 @@
 package com.fisherman.fish.dto;
 
+import com.fisherman.fish.entity.FileEntity;
 import com.fisherman.fish.entity.GmoolEntity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 public class GmoolDTO {
     private long id; // 그물 id
     private String gmoolName; // 그물명
@@ -17,11 +20,25 @@ public class GmoolDTO {
     private LocalDateTime createdTime; // 생성시각
     private int dueMinute; // 유효기간 (분)
     private int pinNumber; // 그물 핀번호
+    
+    private List<FileDTO> fileDTOList; // 첨부 파일
     private int fileCount; // 첨부 파일 수
     // TODO: QR코드 이미지 저장?
 
     public static GmoolDTO toGmoolDTO(GmoolEntity gmoolEntity) {
-
+        GmoolDTO gmoolDTO = new GmoolDTO();
+        gmoolDTO.setId(gmoolEntity.getId());
+        gmoolDTO.setGmoolName(gmoolEntity.getGmoolName());
+        gmoolDTO.setPassword(gmoolEntity.getPassword());
+        gmoolDTO.setCreatedTime(gmoolEntity.getCreatedTime());
+        gmoolDTO.setDueMinute(gmoolEntity.getDueMinute());
+        gmoolDTO.setPinNumber(gmoolEntity.getPinNumber());
+        List<FileEntity> fileEntityList = gmoolEntity.getFileEntityList();
+        List<FileDTO> fileDTOList = new ArrayList<>();
+        for(FileEntity f : fileEntityList)fileDTOList.add(FileDTO.toFileDTO(f));
+        gmoolDTO.setFileDTOList(fileDTOList);
+        gmoolDTO.setFileCount(gmoolEntity.getFileCount());
+        return gmoolDTO;
     }
 
 }

3eb6764a6c8b0405fe407b8a480094c6f8838e65
Jaehwan Hyun <jec0101@naver.com>
2024-01-15 22:20:16 +0900

    GmoolService 업데이트에 따른 수정 - createGmool:   핀번호 생성은 이제 GmoolService에서 이뤄짐

--- back-end/src/main/java/com/fisherman/fish/controller/GmoolController.java
+++ back-end/src/main/java/com/fisherman/fish/controller/GmoolController.java
@@ -1,6 +1,5 @@
 package com.fisherman.fish.controller;
 
-import com.fisherman.fish.dto.FileDTO;
 import com.fisherman.fish.dto.GmoolDTO;
 import com.fisherman.fish.service.GmoolService;
 import com.fisherman.fish.service.MemberService;
@@ -8,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,37 +19,25 @@ public class GmoolController {
     public List<GmoolDTO> getGmools(){
         // 모든 그물 반환
         List<GmoolDTO> gmools = gmoolService.findAll();
-        // TODO: 검색 결과 비어있다면 예외처리?
-
         return gmools;
     }
     
     @PostMapping("/")
-    public String createGmool(@ModelAttribute("gmool") GmoolDTO gmoolDTO, @ModelAttribute("file") FileDTO file){
-        // 받은 정보로 그물 생성
-        //  1. pin 번호 생성
-        //  2. qr코드 생성
-        //  3. 링크 생성
-        //  셋 다 묶어서 반환
-        // 해당 그물 저장
-        int pinNumber = GmoolService.generatePinNumber();
-        gmoolDTO.setPinNumber(pinNumber);
-        // TODO: 첨부 파일 수를 여기서 세서 그물dto에 채워줘야 할듯
-        // qr코드랑 링크 생성했다고 가정
-        gmoolService.save(gmoolDTO);
-        return "ok";
+    public GmoolDTO createGmool(@ModelAttribute("gmool") GmoolDTO gmoolDTO){
+        // 예외처리 : DTO 없이 POST 요청 보낸 경우
+        if(gmoolDTO == null){
+            return null;
+        }
+        GmoolDTO savedDTO = gmoolService.save(gmoolDTO);
+        return savedDTO;
     }
 
     @GetMapping("/{gid}")
-    public String getGmool(@PathVariable(name="gid") Long gid){
+    public Object getGmool(@PathVariable(name="gid") Long gid){
         // 해당 그물 반환
         GmoolDTO gmool = gmoolService.findById(gid);
-        if(gmool == null){
-            return "yes";
-        }
-        else{
-            return "no";
-        }
+        if(gmool == null) return "no gmool for you!";
+        return gmool;
     }
 
     @GetMapping("/{gid}/files")

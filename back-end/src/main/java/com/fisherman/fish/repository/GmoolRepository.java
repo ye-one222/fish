3cc5ae644afc0ca17a27b9860661ea72bc524342
Jaehwan Hyun <jec0101@naver.com>
2024-01-15 19:05:13 +0900

    검색 메서드 추가 - findByMemberId

--- back-end/src/main/java/com/fisherman/fish/repository/GmoolRepository.java
+++ back-end/src/main/java/com/fisherman/fish/repository/GmoolRepository.java
@@ -3,6 +3,9 @@ package com.fisherman.fish.repository;
 import com.fisherman.fish.entity.GmoolEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GmoolRepository extends JpaRepository<GmoolEntity, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface GmoolRepository extends JpaRepository<GmoolEntity, Long> {
+    Optional<List<GmoolEntity>> findByMemberId(String userID);
 }

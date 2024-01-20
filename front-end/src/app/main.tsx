31cba672d2aeee3c91f98952acea89c404672b20
Jeongbowoon <happyanne200212@gmail/com>
2024-01-20 14:17:21 +0900

    잘 클론되었나 확인

--- front-end/src/app/main.tsx
+++ front-end/src/app/main.tsx
@@ -2,7 +2,10 @@
 import React from 'react'
 
 export const MainPage:React.FC=()=>{
-    return<div>
-        <button>mainpage btn </button>
-    </div>
+    return<main style={{backgroundColor:'#E8FAFD', height:'100vh',width:'100vh'}} className='min-h-screen max-h-screen h-screen w-full flex items-center justify-center'>
+        <div>
+            <button className='color-red-400'>mainpage btn </button>
+            <h1 style={{color:'red', backgroundColor:'black'}} >h1</h1>
+        </div>
+    </main>
 }

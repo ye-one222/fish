c5d0f0a88d07e9b12393d444482af8661c4a98db
Jeongbowoon <happyanne200212@gmail/com>
2024-01-22 21:10:38 +0900

    main page 대충만듦

--- front-end/src/router/index.tsx
+++ front-end/src/router/index.tsx
@@ -1,16 +1,23 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import React from "react";
 import { MainPage } from "../app/main"
+import { DownLoadPage } from '../app/download';
+import "/node_modules/react-grid-layout/css/styles.css";
+import "/node_modules/react-resizable/css/styles.css";
 //import { RouterProvider } from "react-router-dom";
 
-export type DiaryRouterPath = '/' | `/detail/${string}` | '/emotions' | `/emotions/${string}`
-const diaryRouter = createBrowserRouter([
+export type FishRouterPath = '/' | '/download' 
+const fishRouter = createBrowserRouter([
     {
         path: '/',
         element: <MainPage />,
     },
+    {
+        path:'/download',
+        element: <DownLoadPage />,
+    },
 ])
 
 // eslint-disable-next-line react/jsx-no-undef
-export const DiaryRouter = () => <RouterProvider router={diaryRouter} />
+export const FishRouter = () => <RouterProvider router={fishRouter} />
 

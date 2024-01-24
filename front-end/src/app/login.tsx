de25f6c28efaf2846ee7fc43dd3ca64fff41248f
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2024-01-25 02:22:15 +0900

    upload 페이지 ui 2차 수정

--- front-end/src/app/login.tsx
+++ front-end/src/app/login.tsx
@@ -4,44 +4,42 @@ import { BackToMain } from '../interface/back.tsx'
 
 export const LoginPage:React.FC=()=>{
     return (
-        //<div className="bg-white flex flex-row justify-center w-full">
-            <div className="bg-white h-full flex flex-col items-center justify-center">
-                {/* title */}
-                <div className="flex items-center [font-family:'Inter-SemiBold',Helvetica] font-semibold text-[#27416d] text-[70px] text-center tracking-[0] leading-[normal]">
-                    Let's go catch fish
-                </div>
-
-                {/* login form */}
-                <form className="flex flex-col w-full items-center justify-center">
-                    <input
-                        required
-                        type="text"
-                        placeholder="ID"
-                        className="w-1/3 mt-20 [font-family:'Inter-Regular',Helvetica] font-normal text-[#818da2] text-[20px] tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-6 py-5"
-                    />
-                    <input
-                        required
-                        type="text"
-                        placeholder="Password"
-                        className="w-1/3 mt-8 [font-family:'Inter-Regular',Helvetica] font-normal text-[#818da2] text-[20px] tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-6 py-5"
-                    />
-
-                    <button
-                        type="submit" 
-                        className="mt-10 [font-family:'Inter-Medium',Helvetica] font-medium text-[#27416d] text-[30px] text-center tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-20 py-4"
-                    >
-                        Log In
-                    </button>
-                </form>
-                <Link to="/signup" className="mt-8">
-                    <div className="[font-family:'Inter-Regular',Helvetica] font-normal text-[#27416d] text-[18px] text-center tracking-[0] leading-[normal]">
-                        회원가입
-                    </div>
-                </Link>
-                
-                {/* 좌측 상단 - 돌아가기 */}
-                <BackToMain />
+        <div className="bg-white h-full flex flex-col items-center justify-center">
+            {/* title */}
+            <div className="flex items-center [font-family:'Inter-SemiBold',Helvetica] font-semibold text-[#27416d] text-[70px] text-center tracking-[0] leading-[normal]">
+                Let's go catch fish
             </div>
-        //</div>
+
+            {/* login form */}
+            <form className="flex flex-col w-full items-center justify-center">
+                <input
+                    required
+                    type="text"
+                    placeholder="ID"
+                    className="w-1/3 mt-20 [font-family:'Inter-Regular',Helvetica] font-normal text-[#818da2] text-[20px] tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-6 py-5"
+                />
+                <input
+                    required
+                    type="text"
+                    placeholder="Password"
+                    className="w-1/3 mt-8 [font-family:'Inter-Regular',Helvetica] font-normal text-[#818da2] text-[20px] tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-6 py-5"
+                />
+
+                <button
+                    type="submit" 
+                    className="mt-10 [font-family:'Inter-Medium',Helvetica] font-medium text-[#27416d] text-[30px] text-center tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-20 py-4"
+                >
+                    Log In
+                </button>
+            </form>
+            <Link to="/signup" className="mt-8">
+                <div className="[font-family:'Inter-Regular',Helvetica] font-normal text-[#27416d] text-[18px] text-center tracking-[0] leading-[normal]">
+                    회원가입
+                </div>
+            </Link>
+            
+            {/* 좌측 상단 - 돌아가기 */}
+            <BackToMain />
+        </div>
     )
 }
--- front-end/src/app/upload/upload.tsx
+++ front-end/src/app/upload/upload.tsx
@@ -1,98 +1,121 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../tailwind.css';
 import { BackToMain } from '../../interface/back.tsx';
 
-export const UploadPage:React.FC=()=>{
+const UploadedFile:React.FC=()=>{
     return (
-    <div className="bg-white flex flex-row justify-center w-full">
-        <div className="bg-white overflow-auto w-[1512px] h-[982px] relative">
+        <div className="relative w-[394px] h-[61px] bg-[#cdf1f7] rounded-[50px]">
+            <div className="absolute w-[303px] h-[61px] top-0 left-[18px]">
+                <div className="w-[301px] h-[40px] top-3 left-[2px] font-bold text-[#000000cc] text-right absolute [font-family:'Inter',Helvetica] text-[30px] tracking-[0] leading-[normal]">
+                    Lorem Ipsum.jpg
+                </div>
+                <div className="w-[49px] h-[48px] top-[6px] left-0 bg-[#d9d9d9] absolute rounded-[50px]" />
+                <div className="absolute w-[47px] h-[21px] top-[20px] left-[2px] [font-family:'Inter',Helvetica] font-bold text-[#8a8282] text-[16px] text-center tracking-[0] leading-[normal]">
+                    jpg
+                </div>
+            </div>
+            <div className="absolute h-[36px] top-[13px] left-[354px] [font-family:'Inter',Helvetica] font-bold text-[#ff00004c] text-[30px] text-right tracking-[0] leading-[normal] whitespace-nowrap">
+                X
+            </div>
+        </div>
+    )
+}
+
+function Dropdown() {
+    return (
+      <>
+        <li>마이페이지</li>
+        <li>로그아웃</li>
+      </>
+    );
+}
+
+export const UploadPage:React.FC=()=>{
+    const [view, setView] = useState(false);
+    
+    return (
+        <div className="bg-white h-full flex flex-col items-center">
             {/* title */}
-            <div className="w-[514px] h-[138px] top-[8px] left-[497px] font-bold text-[50px] text-center absolute [font-family:'Inter',Helvetica] text-[#27416d] tracking-[0] leading-[normal]">
+            <div className="flex mt-8 font-bold text-[50px] text-center [font-family:'Inter',Helvetica] text-[#27416d] tracking-[0] leading-[normal]">
                 새로운 FISH 만들기
             </div>
+            
+            <div className="flex flex-row w-full h-full justify-center">
+                {/* 왼쪽 박스 */}
+                <div className="flex flex-col items-center h-2/3 w-1/3 max-w-[521px] mt-10 mr-20 p-6 bg-[#e7fafc] rounded-[50px]">
+                    <div className="w-full h-3/4 flex flex-col items-center bg-[#f7fdff] rounded-[50px] border-2 border-dashed border-[#27416d]">
+                        <div className="bg-black">
 
-            {/* 왼쪽 박스 */}
-            <div className="absolute w-[521px] h-[592px] top-[221px] left-[181px] bg-[#e7fafc] rounded-[50px]">
-                <div className="absolute w-[451px] h-[414px] top-[33px] left-[35px] bg-[#f7fdff] rounded-[50px] border border-dashed border-[#27416d]">
-                    <div className="absolute w-[400px] h-[61px] top-[39px] left-[28px]">
-                        <div className="relative w-[394px] h-[61px] bg-[#cdf1f7] rounded-[50px]">
-                            <div className="absolute w-[303px] h-[61px] top-0 left-[18px]">
-                                <div className="w-[301px] h-[40px] top-3 left-[2px] font-bold text-[#000000cc] text-right absolute [font-family:'Inter',Helvetica] text-[30px] tracking-[0] leading-[normal]">
-                                    Lorem Ipsum.jpg
-                                </div>
-                                <div className="w-[49px] h-[48px] top-[6px] left-0 bg-[#d9d9d9] absolute rounded-[50px]" />
-                                <div className="absolute w-[47px] h-[21px] top-[20px] left-[2px] [font-family:'Inter',Helvetica] font-bold text-[#8a8282] text-[16px] text-center tracking-[0] leading-[normal]">
-                                    jpg
-                                </div>
-                            </div>
-                            <div className="absolute h-[36px] top-[13px] left-[354px] [font-family:'Inter',Helvetica] font-bold text-[#ff00004c] text-[30px] text-right tracking-[0] leading-[normal] whitespace-nowrap">
-                                X
-                            </div>
+                            {/* 파일들 */}
+
+                        </div>
+                        <div className="mt-[200px] w-[232px] h-[57px] font-normal text-[#27416d5e] text-[20px] text-center absolute [font-family:'Inter',Helvetica] tracking-[0] leading-[normal]">
+                            공유할 파일을 <br />
+                            끌어주세요
+                        </div>
+                        <img className="w-[158px] h-[158px] mt-[60px]" alt="Image" src="/img/image.png" />
+                    </div>
+                    <button className="flex w-1/4 max-w-[500px] items-center justify-center mt-[300px] bg-[#f7fdff] font-medium text-[#27416d5e] text-[30px] text-center absolute [font-family:'Inter',Helvetica] tracking-[0] leading-[normal] rounded-[50px] p-5">
+                        파일 선택
+                    </button>                
+                </div>
+
+                {/* 오른쪽 박스 */}
+                <div className="flex flex-col">
+                    <div className="flex flex-row justify-between mt-12">
+                        <div className="[font-family:'Inter',Helvetica] font-medium text-[#27416d] text-[30px] tracking-[0] leading-[normal]">
+                            FISH 명
+                        </div>
+                        <div className="mt-4 font-medium text-[15px] [font-family:'Inter',Helvetica] text-[#27416d] tracking-[0] leading-[normal]">
+                            선택사항
                         </div>
                     </div>
-                    <div className="w-[232px] h-[57px] top-[310px] left-[107px] font-normal text-[#27416d5e] text-[30px] text-center absolute [font-family:'Inter',Helvetica] tracking-[0] leading-[normal]">
-                        공유할 파일을 <br />
-                        끌어주세요
+                    <input
+                    required
+                    type="text"
+                    className="w-full m-2 [font-family:'Inter-Regular',Helvetica] font-normal text-[#818da2] text-[20px] tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-6 py-5"
+                    />
+
+                    <div className="flex flex-row justify-between mt-8">
+                        <div className="[font-family:'Inter',Helvetica] font-medium text-[#27416d] text-[30px] tracking-[0] leading-[normal]">
+                            암호 설정
+                        </div>
+                        <div className="mt-4 font-medium text-[15px] [font-family:'Inter',Helvetica] text-[#27416d] tracking-[0] leading-[normal]">
+                            선택사항
+                        </div>
                     </div>
-                    <img className="absolute w-[158px] h-[158px] top-[132px] left-[146px]" alt="Image" src="/img/image.png" />
-                </div>
-                <button className="flex absolute w-[435px] h-[94px] top-[478px] left-[39px]">
-                    <div className="w-[435px] h-[86px] top-[2px] left-0 bg-[#f7fdff] absolute rounded-[50px]" />
-                    <div className="flex items-center justify-center w-[232px] h-[86px] top-0 left-[92px] font-medium text-[#27416d5e] text-[30px] text-center absolute [font-family:'Inter',Helvetica] tracking-[0] leading-[normal]">
-                        파일 선택
-                    </div>
-                </button>                
-            </div>
+                    <input
+                    required
+                    type="text"
+                    className="w-full m-2 [font-family:'Inter-Regular',Helvetica] font-normal text-[#818da2] text-[20px] tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-6 py-5"
+                    />
 
-            {/* 오른쪽 박스 */}
-            <div className="absolute w-[647px] h-[129px] top-[213px] left-[782px]">
-                <div className="absolute w-[237px] h-[86px] top-5 left-[9px] [font-family:'Inter',Helvetica] font-medium text-[#27416d] text-[30px] tracking-[0] leading-[normal]">
-                    FISH 명
-                </div>
-                <div className="w-[128px] h-[45px] top-[36px] left-[519px] font-medium text-[20px] absolute [font-family:'Inter',Helvetica] text-[#27416d] tracking-[0] leading-[normal]">
-                    선택사항
-                </div>
-                <div className="w-[601px] h-[60px] top-[69px] left-0 bg-[#e8fafd] absolute rounded-[50px]" />
-                <div className="w-[602px] h-[40px] top-[79px] left-[34px] font-medium text-[#0000005e] absolute [font-family:'Inter',Helvetica] text-[30px] tracking-[0] leading-[normal]">
-                    Lorem Ipsum
-                </div>
-            </div>
+                    <div className="flex flex-row mt-20">
+                        <div className="flex flex-col">
+                            <div className="[font-family:'Inter',Helvetica] font-medium text-[#27416d] text-[20px] tracking-[0] leading-[normal]">
+                                유효기간
+                            </div>
+                            <ul
+                            className="w-full m-2 [font-family:'Inter-Regular',Helvetica] font-normal text-[#818da2] text-[20px] tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-6 py-5" 
+                            onClick={() => {setView(!view)}}>
+                                1시간{" "}
+                                {view? '^':'⌄'}
+                                {view && <Dropdown />}
+                            </ul>
+                        </div>
 
-            <div className="absolute w-[647px] h-[129px] top-[362px] left-[782px]">
-                <div className="w-[128px] h-[45px] top-[36px] left-[519px] font-medium text-[20px] absolute [font-family:'Inter',Helvetica] text-[#27416d] tracking-[0] leading-[normal]">
-                    선택사항
-                </div>
-                <div className="absolute w-[237px] h-[86px] top-5 left-[9px] [font-family:'Inter',Helvetica] font-medium text-[#27416d] text-[30px] tracking-[0] leading-[normal]">
-                    암호 설정
-                </div>
-                <div className="w-[601px] h-[60px] top-[69px] left-0 bg-[#e8fafd] absolute rounded-[50px]" />
-                <div className="w-[602px] h-[40px] top-[79px] left-[34px] font-medium text-[#0000005e] text-[30px] absolute [font-family:'Inter',Helvetica] tracking-[0] leading-[normal]">
-                    ●●●●●
-                </div>
-            </div>
-
-            <div className="absolute w-[184px] h-[123px] top-[690px] left-[791px]">
-                <div className="w-[131px] h-[78px] top-5 left-[25px] font-medium text-[30px] absolute [font-family:'Inter',Helvetica] text-[#27416d] tracking-[0] leading-[normal]">
-                    유효기간
-                </div>
-                <div className="w-[184px] h-[59px] top-[64px] left-0 bg-[#e8fafd] absolute rounded-[50px]" />
-                <div className="absolute w-[112px] h-[42px] top-[71px] left-[35px] [font-family:'Inter',Helvetica] font-medium text-[#0000005e] text-[30px] tracking-[0] leading-[normal]">
-                    1 시간
-                </div>
-                <img className="absolute w-[35px] h-[25px] top-[83px] left-[125px]" alt="Polygon" src="/img/polygon-1.svg" />
-            </div>
-
-            <button className="absolute w-[313px] h-[89px] top-[732px] left-[1064px]">
-                <div className="flex justify-center items-center relative w-[311px] h-[89px] top-[2px] left-0 bg-[#E8FAFD] rounded-[50px]">
-                    <div className="flex justify-center items-center h-[61px] top-[14px] left-[87px] font-bold text-[#27416dcc] text-[50px] text-center absolute [font-family:'Inter',Helvetica] tracking-[0] leading-[normal]">
-                        만들기
+                        <button
+                            type="submit" 
+                            className="ml-10 mt-[40px] mb-[8px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#27416d] text-[30px] text-center tracking-[0] leading-[normal] bg-[#E8FAFD] rounded-[50px] px-8"
+                        >
+                            만들기
+                        </button>
                     </div>
                 </div>
-            </button>
-            
-            {/* 좌측 상단 - 돌아가기 */}
-            <BackToMain />
+
+                {/* 좌측 상단 - 돌아가기 */}
+                <BackToMain />
+            </div>
         </div>
-    </div>
     )
 }

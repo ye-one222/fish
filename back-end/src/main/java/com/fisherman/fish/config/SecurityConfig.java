4af2774de361253cc53bb889cd447282fed45aa3
Jaehwan Hyun <jec0101@naver.com>
2024-04-01 03:17:31 +0900

    feat : 필터체인에 fishFilter추가

--- back-end/src/main/java/com/fisherman/fish/config/SecurityConfig.java
+++ back-end/src/main/java/com/fisherman/fish/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.fisherman.fish.config;
 
 import com.fisherman.fish.filter.FishFilter;
+import com.fisherman.fish.service.FishService;
 import com.fisherman.fish.utility.JWTUtil;
 import com.fisherman.fish.filter.LoginFilter;
 import jakarta.servlet.http.HttpSession;
@@ -26,20 +27,22 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 public class SecurityConfig {
     private final AuthenticationConfiguration configuration;
     private final JWTUtil jwtUtil;
+    private final FishService fishService;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         LoginFilter loginFilter = new LoginFilter(authenticationManager(configuration), jwtUtil);
         loginFilter.setFilterProcessesUrl("/users/login");
-        FishFilter fishFilter = new FishFilter();
+        FishFilter fishFilter = new FishFilter(jwtUtil, fishService);
+        fishFilter.setFilterProcessesUrl("/fishes/[0-9]+");
         http
                 .authorizeHttpRequests((authorizedHttpRequests) -> authorizedHttpRequests
                         .requestMatchers("/h2-console/**").permitAll() // h2 콘솔 허용
                         .requestMatchers("/users/login", "/users/signup").permitAll() // 로그인, 회원 가입 경로 허용
                         .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/users/{}/gmools").permitAll()/*.hasAnyRole()*/
+                        .requestMatchers(HttpMethod.GET, "/users/{}/fishes").permitAll()/*.hasAnyRole()*/
                         /*.requestMatchers(HttpMethod.GET,
-                                "/gmool", "/users", "/users/{userId}").hasRole("ADMIN")*/ // 나중에 주석 해제
+                                "/fishes", "/users", "/users/{userId}").hasRole("ADMIN")*/ // 나중에 주석 해제
                         .anyRequest().permitAll()
                 )
                 // csrf는 필요 없다
